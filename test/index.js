c4fcd3e7174691269a33597c990315342bc20463
geek <wpreul@gmail.com>
2018-04-23 18:49:07 -0500

    Reduce scope of auth cookie

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Path = require('path');
+const Boom = require('boom');
 const Code = require('code');
 const Hapi = require('hapi');
 const Lab = require('lab');
@@ -30,7 +31,7 @@ it('can be registered with hapi', async () => {
   await server.initialize();
 });
 
-it('will protect a route', async () => {
+it('allows access to a route for valid cloudapi accounts', async () => {
   const account = {
     id: 'b89d9dd3-62ce-4f6f-eb0d-f78e57d515d9',
     login: 'barbar',
@@ -93,6 +94,132 @@ it('will protect a route', async () => {
   expect(res.headers.location).to.contain('sig=');
 
   const authRes = await server.inject('/?token=something');
+  expect(authRes.payload).to.equal(account.id);
+  await sdcServer.stop();
+  await apiServer.stop();
+});
+
+it('prevents access to a route for invalid cloudapi accounts', async () => {
+  const apiServer = Hapi.server();
+  apiServer.route({
+    method: 'GET',
+    path: '/my',
+    handler: function (request, h) {
+      return Boom.unauthorized();
+    }
+  });
+
+  await apiServer.start();
+
+  const sdcServer = Hapi.server();
+  sdcServer.route({
+    method: 'GET',
+    path: '/session',
+    handler: (request, h) => {
+      return { uuid: 'foo' };
+    }
+  });
+  await sdcServer.start();
+
+  const server = Hapi.server();
+  const options = {
+    baseUrl: 'http://localhost',
+    ssoUrl: `http://localhost:${sdcServer.info.port}`,
+    apiBaseUrl: `http://localhost:${apiServer.info.port}`,
+    permissions: { portal: true },
+    keyPath,
+    keyId
+  };
+
+  await server.register({ plugin: SSO, options });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+      auth: 'sso',
+      handler: function (request, h) {
+        return '';
+      }
+    }
+  });
+
+  await server.initialize();
+  const res = await server.inject('/');
+  expect(res.statusCode).to.equal(302);
+  expect(res.headers.location).to.contain('sig=');
+
+  const authRes = await server.inject('/?token=something');
+  expect(authRes.statusCode).to.equal(302);
+  await sdcServer.stop();
+  await apiServer.stop();
+});
+
+it('allows access to a route for a valid sub user', async () => {
+  const account = {
+    id: 'b89d9dd3-62ce-4f6f-eb0d-f78e57d515d9',
+    login: 'barbar',
+    email: 'barbar@example.com',
+    companyName: 'Example Inc',
+    firstName: 'BarBar',
+    lastName: 'Jinks',
+    phone: '123-456-7890',
+    updated: '2015-12-21T11:48:54.884Z',
+    created: '2015-12-21T11:48:54.884Z'
+  };
+
+  const apiServer = Hapi.server();
+  apiServer.route({
+    method: 'GET',
+    path: '/my',
+    handler: function (request, h) {
+      return account;
+    }
+  });
+
+  await apiServer.start();
+
+  const sdcServer = Hapi.server();
+  sdcServer.route({
+    method: 'GET',
+    path: '/session',
+    handler: (request, h) => {
+      return { uuid: 'foo' };
+    }
+  });
+  await sdcServer.start();
+
+  const server = Hapi.server();
+  const options = {
+    baseUrl: 'http://localhost',
+    ssoUrl: `http://localhost:${sdcServer.info.port}`,
+    apiBaseUrl: `http://localhost:${apiServer.info.port}`,
+    permissions: { portal: true },
+    keyPath,
+    keyId
+  };
+
+  await server.register({ plugin: SSO, options });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+      auth: 'sso',
+      handler: function (request, h) {
+        expect(request.auth.credentials.token[0]).to.equal('something');
+        expect(request.auth.credentials.token[1]).to.equal('somethingelse');
+        return request.auth.credentials.profile.id;
+      }
+    }
+  });
+
+  await server.initialize();
+  const res = await server.inject('/');
+  expect(res.statusCode).to.equal(302);
+  expect(res.headers.location).to.contain('sig=');
+
+  const authRes = await server.inject('/?token=something&token=somethingelse');
   expect(authRes.payload).to.equal(account.id);
   await sdcServer.stop();
   await apiServer.stop();
